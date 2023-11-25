cbbfb33b00b6a656a8fbfa48527ee3d40575bb55
EzerKadang <ezerkadang@gmail.com>
2023-11-25 21:47:32 +0800

    selesai belajar pengulangan while

---
+++ Pengulangan/While/angkot1.js
@@ -0,0 +1,16 @@
+//! latihan while
+// let angkot = 1;
+
+// while (angkot <= 10) {
+//   console.log(`angkot No. ${angkot} beroprasi dengan baik`);
+//   angkot++;
+// }
+
+// ? versi lain
+let jumlahAngkot = 10;
+let noAngkot = 1;
+
+while (noAngkot <= jumlahAngkot) {
+  console.log(`angkot No. ${noAngkot} beroprasi dengan baik`);
+  noAngkot++;
+}
--- Pengulangan/angkot1.js
+++
@@ -1,7 +0,0 @@
-//! latihan while
-let angkot = 1;
-
-while (angkot <= 10) {
-  console.log(`angkot No. ${angkot} beroprasi dengan baik`);
-  angkot++;
-}
