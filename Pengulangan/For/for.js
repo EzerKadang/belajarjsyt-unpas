90664c9bd27237ca619f1ec87ad768a66b40ef47
EzerKadang <ezerkadang@gmail.com>
2023-11-25 22:19:15 +0800

    selesai belajar pengulangan for

---
+++ Pengulangan/For/for.js
@@ -0,0 +1,8 @@
+// !DOM looping for
+// for(penentuan nilai awal; penentuan kondisi; dicrement/increment){
+//     aksi
+// }
+
+for (let noAngkot = 1; noAngkot <= 10; noAngkot++) {
+  console.log(`Angkot no. ${noAngkot} berjalan dengan baik`);
+}
