9f54c65fe37d93f13ce48efd01f8c1c31a94a72b
EzerKadang <ezerkadang@gmail.com>
2023-12-04 21:01:17 +0800

    belajar argument dan parameter

---
+++ function/Membuat dan menjalankan func/Kubus.js
@@ -0,0 +1,11 @@
+function luasKubus(a, b) {
+  let sisi1, sisi2, total;
+
+  sisi1 = a ** 3;
+  sisi2 = b ** 3;
+  total = sisi1 + sisi2;
+
+  return total;
+}
+
+console.log(`luas nya adalah ${luasKubus(4, 3)}`);
---
+++ function/Prameter dan argument/script.js
@@ -0,0 +1,62 @@
+//! contoh 1
+// function tambah(a, b) {
+//   return a + b;
+// }
+
+// console.log(tambah(1, 2));
+
+//! contoh 2
+// function tambah(a, b) {
+//   return a + b;
+// }
+
+// var hasil = tambah(1, 2);
+
+// console.log(hasil);
+
+//! contoh 3 : inputan angka menggunakan expresion
+// function tambah(a, b) {
+//   return a + b;
+// }
+
+// var a = 10;
+// var b = 20;
+// var hasil = tambah(a, b);
+
+// console.log(hasil);
+
+//! contoh 4 : inputan angka dari user
+// function tambah(a, b) {
+//   return a + b;
+// }
+
+// var a = parseInt(prompt(`masukan nilai 1 :`));
+// var b = parseInt(prompt(`masukan nilai 2 :`));
+// var hasil = tambah(a, b);
+
+// console.log(hasil);
+
+//! Contoh 5 : Argument menggunakan function
+// function tambah(a, b) {
+//   return a + b;
+// }
+
+// function kali(a, b) {
+//   return a * b;
+// }
+
+// var hasil = kali(tambah(1, 2), tambah(1, 2));
+
+// console.log(hasil);
+
+//! Contoh 6 : tidak menggunakan prameter
+function tambah() {
+  let hasil = 0;
+  for (let i = 0; i < arguments.length; i++) {
+    hasil += arguments[i];
+  }
+  return hasil;
+}
+
+var coba = tambah(1, 2, 3, 4);
+console.log(coba);
