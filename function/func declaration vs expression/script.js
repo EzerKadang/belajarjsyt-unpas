78253abdb20a65c998e08ecee15c7cdf7c96a42c
EzerKadang <ezerkadang@gmail.com>
2023-12-04 22:03:53 +0800

    belajar func declaration VS expression

---
+++ function/func declaration vs expression/script.js
@@ -0,0 +1,14 @@
+// ! DOM Func DECLARATION
+// ? Function Identifier (ParameterList Opt) {FunctionBody}
+// ! DOM Func EXPRESSION
+// ? Function Identifier Opt (ParameterList Opt) {FunctionBody}
+
+//! Contoh Func Declaration
+// function tampilPesan(nama) {
+//   alert(`halo ${nama}`);
+// }
+
+//! Contoh Func Expression
+// let tampilPesan = function (nama) {
+//   alert(`halo ${nama}`);
+// };
