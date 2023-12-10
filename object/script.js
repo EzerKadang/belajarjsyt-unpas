7b24a8d0a701190e49b055c6c40a4b6111019263
EzerKadang <ezerkadang@gmail.com>
2023-12-10 13:18:28 +0800

    belajar dasar javascript selesai

--- object/script.js
+++ object/script.js
@@ -1,4 +1,4 @@
-// Example 1
+//! Example 1
 // let mahasiswa = {
 //   nama: `eser`,
 //   lulus: true,
@@ -15,45 +15,73 @@
 
 // console.log(mahasiswa.ipKomulatif());
 
-// membuat object
-var mhs = {
-  nama: `eser`,
-  lulus: true,
-  ipSemester: [2.9, 3.1, 3.25, 4.0, 3.2],
-  alamat: {
-    jalan: `poros kabo`,
-    gang: `sekawan`,
-    no: 55,
-  },
-};
+// //! membuat object
+// var mhs = {
+//   nama: `eser`,
+//   lulus: true,
+//   ipSemester: [2.9, 3.1, 3.25, 4.0, 3.2],
+//   alamat: {
+//     jalan: `poros kabo`,
+//     gang: `sekawan`,
+//     no: 55,
+//   },
+// };
 
-// membuat object secara litaral
-var mahasiswa = {
-  nama: "eser",
-  nrp: "1234555",
-  email: "ezer@gmail.com",
-  jurusan: "informatika",
-};
+// //! membuat object secara litaral
+// var mahasiswa = {
+//   nama: "eser",
+//   nrp: "1234555",
+//   email: "ezer@gmail.com",
+//   jurusan: "informatika",
+// };
 
-// object declaration
-function functionmhs(nama, nrp, email, jurusan) {
-  var mhs = {};
-  mhs.nama = nama;
-  mhs.nrp = nrp;
-  mhs.email = email;
-  mhs.jurusan = jurusan;
-  return mhs;
+// //! object declaration
+// function functionmhs(nama, nrp, email, jurusan) {
+//   var mhs = {};
+//   mhs.nama = nama;
+//   mhs.nrp = nrp;
+//   mhs.email = email;
+//   mhs.jurusan = jurusan;
+//   return mhs;
+// }
+
+// var mhs2 = functionmhs("ellys", "1234555", "ellys@gmail.com", "bisnis");
+// var mhs3 = functionmhs("eser", "123123123", "eser@gmail.com", "informatika");
+
+// //! constructor
+// function Mhs(nama, nrp, email, jurusan) {
+//   this.nama = nama;
+//   this.nrp = nrp;
+//   this.email = email;
+//   this.jurusan = jurusan;
+// }
+
+// var mhs4 = new Mhs("tison", "123123", "tison@gmail.com", "industri");
+
+//! object this
+// ? object function declaration
+function halo() {
+  console.log(this);
+  console.log("halo");
 }
 
-var mhs2 = functionmhs("ellys", "1234555", "ellys@gmail.com", "bisnis");
-var mhs3 = functionmhs("eser", "123123123", "eser@gmail.com", "informatika");
+halo();
+//* mengembalikan object global
 
-// constructor
-function Mhs(nama, nrp, email, jurusan) {
-  this.nama = nama;
-  this.nrp = nrp;
-  this.email = email;
-  this.jurusan = jurusan;
+// ? object literal
+var obj = {};
+obj.halo = function () {
+  console.log(this);
+  console.log("halo");
+};
+//* mengembalikan object yang bersangkutan
+
+obj.halo();
+
+function Halo() {
+  console.log(this);
+  console.log("halo");
 }
 
-var mhs4 = new Mhs("tison", "123123", "tison@gmail.com", "industri");
+new Halo();
+//* Mengembalikan object yang baru dibuat
