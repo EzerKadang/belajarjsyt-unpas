9f27238f0d4d208762ac4c98d68044f7d7521f0f
EzerKadang <ezerkadang@gmail.com>
2023-12-15 20:27:40 +0800

    belajar var, let and const

---
+++ Advance Java script/var, let and const/script.js
@@ -0,0 +1,9 @@
+// console.log(angka);
+// var angka = 10;
+
+// IIFE (Immidietly Invoked Function Expression) VS SIAF (Self Invoking Anonymous Function)
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+console.log(i);
