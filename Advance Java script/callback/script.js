bf4c50bc6571388c8f67114a714fbfb5f6bc1b2e
EzerKadang <ezerkadang@gmail.com>
2024-01-12 21:47:45 +0800

    review materi callback

--- Advance Java script/callback/script.js
+++ Advance Java script/callback/script.js
@@ -72,10 +72,51 @@
 // console.log("selesai");
 
 //* using JQuery
-$.ajax({
-  URL: "mahasiswa.json",
-  success: (mhs) => {
-    console.log(mhs);
-  },
-  error: () => {},
-});
+// $.ajax({
+//   URL: "mahasiswa.json",
+//   success: (mhs) => {
+//     console.log(mhs);
+//   },
+//   error: () => {},
+// });
+
+//! Review materi
+// function getDataMahasiswa(url, success, error) {
+//   let xhr = new XMLHttpRequest();
+
+//   xhr.onreadystatechange = function () {
+//     if (xhr.readyState === 4) {
+//       if (xhr.status === 200) {
+//         success(xhr.response);
+//       } else if (xhr.status === 404) {
+//         error();
+//       }
+//     }
+//   };
+
+//   xhr.open("get", url);
+//   xhr.send();
+// }
+
+// getDataMahasiswa(
+//   "mahasiswa.json",
+//   (results) => {
+//     const mhs = JSON.parse(results);
+//     mhs.forEach((m) => console.log(m.nama));
+//   },
+//   () => {}
+// );
+
+// Jquery
+
+// console.log(`mulai`);
+// $.ajax({
+//   url: `mahasiswa.json`,
+//   success: (results) => {
+//     results.forEach((m) => console.log(m.nama));
+//   },
+//   error: (e) => {
+//     console.log(e.responseText);
+//   },
+// });
+// console.log(`selesai`);
