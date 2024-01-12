12e7dc50dcefc29e4bbca1f4f8c5a32936f938c3
EzerKadang <ezerkadang@gmail.com>
2024-01-12 20:59:02 +0800

    learn callback

---
+++ Advance Java script/callback/script.js
@@ -0,0 +1,81 @@
+// ! callback
+// * synchronus callback
+// function hallo(nama) {
+//   alert(`hallo, ${nama}`);
+// }
+
+// function tampilkanPesan(callback) {
+//   const nama = prompt(`masukan nama anda`);
+//   callback(nama);
+// }
+
+// tampilkanPesan(hallo);
+
+// ? or using arrow function
+// function tampilkanPesan(callback) {
+//   const nama = prompt(`masukan nama anda`);
+//   callback(nama);
+// }
+
+// tampilkanPesan((nama) => alert(`hallo, ${nama}`));
+
+// * assynchronus callback
+// const mhs = [
+//   {
+//     nama: "eser",
+//     nrp: "080022333",
+//     email: "eser@gmail.com",
+//     jurusan: "informatika",
+//     idDosenwali: 1,
+//   },
+//   {
+//     nama: "isnyu",
+//     nrp: "080022334",
+//     email: "isnyu@gmail.com",
+//     jurusan: "theology",
+//     idDosenwali: 2,
+//   },
+//   {
+//     nama: "lleys",
+//     nrp: "080022334",
+//     email: "isnyu@gmail.com",
+//     jurusan: "bussines",
+//     idDosenwali: 3,
+//   },
+// ];
+
+// function getDataMahasiswa(url, success, error) {
+//   let xhr = new XMLHttpRequest();
+
+//   xhr.onreadystatechange = function () {
+//     if (this.readyState === 4) {
+//       if (xhr.status === 200) {
+//         success(xhr.response);
+//       } else if (xhr.status === 404) {
+//         error();
+//       }
+//     }
+//   };
+//   xhr.open("get", url);
+//   xhr.send();
+// }
+
+// console.log("mulai");
+// getDataMahasiswa(
+//   "mahasiswa.json",
+//   (results) => {
+//     const mhs = JSON.parse(results);
+//     mhs.forEach((m) => console.log(m.nama));
+//   },
+//   () => {}
+// );
+// console.log("selesai");
+
+//* using JQuery
+$.ajax({
+  URL: "mahasiswa.json",
+  success: (mhs) => {
+    console.log(mhs);
+  },
+  error: () => {},
+});
